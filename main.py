46416f3300c6137e0559d1baaa577cb0787cfd37
jinnacho <74641921+jinnacho@users.noreply.github.com>
2025-06-19 14:37:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title("🎓 학번으로 구글 계정 확인하기")
 
 # CSV 파일 불러오기 (같은 폴더에 있어야 함)
 try:
-    df = pd.read_csv("emails.csv")  # 구글 드라이브에서 가져올 경우 마운트 필요
+    df = pd.read_csv("emails.csv",utf-8)  # 구글 드라이브에서 가져올 경우 마운트 필요
 except FileNotFoundError:
     st.error("⚠️ emails.csv 파일이 존재하지 않습니다. 파일을 같은 폴더에 넣어주세요.")
 else:
